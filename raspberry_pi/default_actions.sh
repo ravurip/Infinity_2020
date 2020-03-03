e1c6d575ed413d0910f7e792e9043b5968c14673
Pradeep Ravuri <pradeeprbvs>
2020-03-03 19:08:12 +0530

    Updating code to install portaudio

--- raspberry_pi/default_actions.sh
+++ raspberry_pi/default_actions.sh
@@ -8,5 +8,17 @@ sudo apt-get install git
 sudo apt-get install libasound-dev
 sudo apt-get install alsa-tools alsa-utils
 
+#install portaudio
+cd /usr/src/
+
+sudo wget http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz
+sudo tar xvfz pa_stable_v190600_20161030.tgz
+
+cd /usr/src/portaudio/
+sudo ./configure
+sudo make
+sudo make install
+sudo ldconfig
+
 # Setting audio output to headset port. 1- headset port, 2- HDMI
 amixer cset numid=3 1
